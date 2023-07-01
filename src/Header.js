deffe23431a4d95cf9bf7d09060bfe14ac71eda7
Waxiriz <placlaroush.taonas.SG1@gmail.com>
2023-07-01 18:16:08 +0200

    sematntic tags and OGP

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import Nav from './Nav.js';
+
+function Header() {
+    return (
+        <header>
+            <img src="/assets/Logo.svg"/>
+            <Nav/>
+        </header>
+    );
+}
+
+export default Header;
