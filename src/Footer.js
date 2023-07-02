25a451bf7637989d7689a4ff218325daa9ef9384
Waxiriz <placlaroush.taonas.SG1@gmail.com>
2023-07-02 17:14:10 +0200

    some first css

--- src/Footer.js
+++ src/Footer.js
@@ -1,56 +1,56 @@
+import './Footer.css';
+
 function Footer() {
     return (
         <footer>
             <img src="/assets/restauranfood.jpg"/>
-            <section>
-                <div>
-                    <h5>Doormat Navigation</h5>
-                    <ul>
-                        <li>
-                            <a href="">Home</a>
-                        </li>
-                        <li>
-                            <a href="">About</a>
-                        </li>
-                        <li>
-                            <a href="">Menu</a>
-                        </li>
-                        <li>
-                            <a href="">Reservations</a>
-                        </li>
-                        <li>
-                            <a href="">Order Online</a>
-                        </li>
-                        <li>
-                            <a href="">Login</a>
-                        </li>
-                    </ul>
-                </div>
+            <div>
+                <h5>Doormat Navigation</h5>
+                <ul>
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="">About</a>
+                    </li>
+                    <li>
+                        <a href="">Menu</a>
+                    </li>
+                    <li>
+                        <a href="">Reservations</a>
+                    </li>
+                    <li>
+                        <a href="">Order Online</a>
+                    </li>
+                    <li>
+                        <a href="">Login</a>
+                    </li>
+                </ul>
+            </div>
 
-                <div>
-                    <h5>Contact</h5>
-                    <ul>
-                        <li>Adress</li>
-                        <li>phone number</li>
-                        <li>email</li>
-                    </ul>
-                </div>
+            <div>
+                <h5>Contact</h5>
+                <ul>
+                    <li>Adress</li>
+                    <li>phone number</li>
+                    <li>email</li>
+                </ul>
+            </div>
 
-                <div>
-                    <h5>Social Media Links</h5>
-                    <ul>
-                        <li>
-                            <a href="">Facebook</a>
-                        </li>
-                        <li>
-                            <a href="">Twitter</a>
-                        </li>
-                        <li>
-                            <a href="">Instagram</a>
-                        </li>
-                    </ul>
-                </div>
-            </section>
+            <div>
+                <h5>Social Media Links</h5>
+                <ul>
+                    <li>
+                        <a href="">Facebook</a>
+                    </li>
+                    <li>
+                        <a href="">Twitter</a>
+                    </li>
+                    <li>
+                        <a href="">Instagram</a>
+                    </li>
+                </ul>
+            </div>
         </footer>
     );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
 import Nav from './Nav.js';
+import './Header.css';
 
 function Header() {
     return (
         <header>
             <img src="/assets/Logo.svg"/>
             <Nav/>
+            <img src='/assets/icon_hamburgermenu_.svg' class='burger'/>
         </header>
     );
 }
